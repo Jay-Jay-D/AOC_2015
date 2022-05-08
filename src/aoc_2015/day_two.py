a43f66f56e4937a07839198bb7f2df183e6bb1a2
Jay-Jay-D <jjdambrosio@gmail.com>
2022-05-07 21:07:29 -0300

    day two green

--- src/aoc_2015/day_two.py
+++ src/aoc_2015/day_two.py
@@ -1,11 +1,18 @@
-from math import prod
+from itertools import combinations
+from pathlib import Path
 
-def get_surface_area(l,w,h):
-    return 2*l*w + 2*w*h + 2*h*l
-
-def get_area_smallest_side_area(*dimensions):
-    return prod(dimensions[:2:1])
+def get_box_papper_and_ribbon(suface_area):
+    [a, b, c] = sorted([int(s) for s in suface_area.split('x')])
+    return (2*a*b + 2*b*c + 2*a*c) + (a*b), (2*a + 2*b) + (a*b*c)
 
 if __name__ == "__main__":
-    print(get_area_smallest_side_area(2,3,4))
-    print(get_area_smallest_side_area(1,1,10))
+    puzzle_input = Path('./src/aoc_2015/day_two_input.txt')
+    gifts = puzzle_input.open().readlines()
+    total_area = 0
+    total_ribbon = 0
+    for side_lengths in gifts:
+        papper, ribbon = get_box_papper_and_ribbon(side_lengths)
+        total_area += papper
+        total_ribbon += ribbon
+    print (f'Total square feet of wrapping paper to order: {total_area}')
+    print (f'Total feet of ribbon to order: {total_ribbon}')
---
+++ tests/day_two_test.py
@@ -0,0 +1,12 @@
+import pytest
+from aoc_2015.day_two import get_box_papper_and_ribbon
+
+suface_area_cases = [
+    pytest.param('2x3x4', 58, 34, id='case 1'),
+    pytest.param('1x1x10', 43, 14, id='case 2'),
+    pytest.param('4x23x21', 1402, 1982, id='case 3'),
+]
+
+@pytest.mark.parametrize("side_lengths,expected_area,expected_ribbon", suface_area_cases)
+def test_get_box_surface_area(side_lengths,expected_area,expected_ribbon):
+    assert get_box_papper_and_ribbon(side_lengths) == (expected_area, expected_ribbon)
