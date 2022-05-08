57268198099ce8df7d9044304cd4024dcdd1260b
Jay-Jay-D <jjdambrosio@gmail.com>
2022-05-08 18:17:59 -0300

    refactor day three

--- src/aoc_2015/day_three.py
+++ src/aoc_2015/day_three.py
@@ -4,14 +4,13 @@ from pathlib import Path
 def deliver_gifts_with_robo_santa(directions):
     santa_gift_coordinates = get_gift_coordinates(directions[::2])
     robo_gift_coordinates = get_gift_coordinates(directions[1::2])
-    houses = set(santa_gift_coordinates.keys()).union(set(robo_gift_coordinates.keys()))
-    return len(houses)
+    return santa_gift_coordinates.union(robo_gift_coordinates)
 
 
 def get_gift_coordinates(directions):
     x=0
     y=0    
-    coordinates = {(x,y):1}
+    coordinates = [(x,y)]
     for direction in directions:
         if direction == '^':
             y+=1
@@ -22,16 +21,11 @@ def get_gift_coordinates(directions):
         else:
             x+=-1
 
-        next_coordinate = (x, y)
-        if next_coordinate in coordinates:
-            coordinates[next_coordinate] += 1
-        else:
-            coordinates[next_coordinate] = 1
-    return coordinates
-    #return sum([1 for house in coordinates.values() if house >= 1])
+        coordinates.append((x, y))
+    return set(coordinates)
 
 if __name__ == "__main__":
     puzzle_input = Path('./src/aoc_2015/day_three_input.txt')
     directions = puzzle_input.open().readline()
     print(f'Houses that received at least one present: {len(get_gift_coordinates(directions))}')
-    print(f'Houses that received at least one present with RoboSanta: {deliver_gifts_with_robo_santa(directions)}')
+    print(f'Houses that received at least one present with RoboSanta: {len(deliver_gifts_with_robo_santa(directions))}')
--- tests/day_three_test.py
+++ tests/day_three_test.py
@@ -5,10 +5,10 @@ direction_cases = [
     pytest.param('>', 2, 2, id='case 1'),
     pytest.param('^>v<', 4, 3, id='case 2'),
     pytest.param('^v^v^v^v^v', 2, 11, id='case 3'),
-    pytest.param('^v', 2, 3, id='case 4'),
+    pytest.param('^v', 2, 3, id='case 4')
 ]
 
 @pytest.mark.parametrize("directions,expected,expected_with_robo", direction_cases)
 def test_gift_delivering(directions,expected,expected_with_robo):
     assert len(get_gift_coordinates(directions)) == expected
-    assert deliver_gifts_with_robo_santa(directions) == expected_with_robo
+    assert len(deliver_gifts_with_robo_santa(directions)) == expected_with_robo
