fd4bd09d44892c8c2da6b1fe596f4087fe9f7144
Jay-Jay-D <jjdambrosio@gmail.com>
2022-06-03 09:17:38 -0300

    Day twenty two WIP

--- src/aoc_2015/day_twenty_two.py
+++ src/aoc_2015/day_twenty_two.py
@@ -10,6 +10,14 @@ class Effect:
     damage: int = 0
     mana: int = 0
 
+    def __add__(self, other):
+        return Effect(
+            armor=self.armor + other.armor,
+            heals=self.heals + other.heals,
+            damage=self.damage + other.damage,
+            mana=self.mana + other.mana,
+        )
+
 
 @dataclass
 class Spell:
@@ -18,31 +26,69 @@ class Spell:
     effect: Effect
 
 
+class Spells:
+    MagicMisile = Spell(name="Magic Misile", mana_cost=53, effect=Effect(damage=4))
+    Drain = Spell(name="Drain", mana_cost=73, effect=Effect(damage=2, heals=2))
+    Poison = Spell(name="Poison", mana_cost=173, effect=Effect(damage=3, turns=6))
+
+
 @dataclass
 class Wizard(Player):
-    spells: list[Spell] = field(default_factory=list)
     mana: int = 500
+    spells: list[Spell] = field(default_factory=list)
+    spell_cast_order: list[str] = field(default_factory=list)
+    cast_counter = 0
+    active_spells: dict[Spell, int] = field(default_factory=dict)
 
     def cast_spell(self):
-        spell = self.spells[0]
-        self.mana -= spell.mana_cost
-        return spell
+        final_effect = Effect()
+        # Update turn count in active spells
+        for spell in self.active_spells:
+            self.active_spells[spell] += 1
+
+        casted_spell = self.spells[self.cast_counter]
+        self.cast_counter += 1
+        self.mana -= casted_spell.mana_cost
+
+        if casted_spell.effect.turns > 1:
+            self.active_spells[casted_spell] = 0
+        else:
+            final_effect += casted_spell.effect
+
+        for spell, effect_turns in self.active_spells.items():
+            if effect_turns == 0:
+                # if effect_turns is zero, do nothing.
+                # The effect will activate in the next turn.
+                continue
+
+            final_effect += spell.effect
+            self.active_spells[spell] += 1
+            # Finally, clean affects after turns count passed.
+            if self.active_spells[spell] == spell.effect.turns:
+                del self.active_spells[spell]
+        return final_effect
 
 
 class MatchV2(Match):
     def attack(self, attacker, defender):
         # Player's turn
         if isinstance(attacker, Wizard):
-            spell = attacker.cast_spell()
-            defender.hp -= max((spell.effect.damage - defender.armor), 1)
+            effects = attacker.cast_spell()
+            defender.hp -= max((effects.damage), 1)
+            attacker.hp += effects.heals
+            attacker.mana += effects.mana
+            attacker.armor += effects.armor
             return
         return super().attack(attacker, defender)
 
 
 if __name__ == "__main__":
     spells = [Spell(name="Magic Misile", mana_cost=53, effect=Effect(damage=4))]
-    player = Wizard(spells=spells)
+    player = Wizard(
+        spells=[Spells.Drain, Spells.MagicMisile], spell_cast_order=["Poison", "Magic Misile"]
+    )
     boss = Player()
     arena = MatchV2(player, boss)
     # Act
-    arena.run_turn()
+    for _ in range(4):
+        arena.run_turn()
--- tests/day_twenty_two_test.py
+++ tests/day_twenty_two_test.py
@@ -1,19 +1,59 @@
+from dataclasses import asdict
+import pytest
 from aoc_2015.day_twenty_one import Player
-from aoc_2015.day_twenty_two import Effect, MatchV2, Spell, Wizard
+from aoc_2015.day_twenty_two import Effect, MatchV2, Spell, Spells, Wizard
+
+spells_cases = [
+    pytest.param(
+        [Spells.MagicMisile],
+        ["Magic Misile"],
+        1,
+        {"Boss": {"hp": 96}, "Player": {"mana": 447}},
+        id="Simple spell case",
+    ),
+    pytest.param(
+        [Spells.Drain],
+        ["Drain"],
+        1,
+        {"Boss": {"hp": 98}, "Player": {"mana": 427, "hp": 102}},
+        id="Double effect spell case",
+    ),
+    pytest.param(
+        [Spells.Drain, Spells.MagicMisile],
+        ["Poison", "Magic Misile"],
+        4,
+        {"Boss": {"hp": 87}, "Player": {"mana": 274, "hp": 98}},
+        id="Poison spell lasting effect case",
+    ),
+]
 
 
-def test_cast_spell():
+@pytest.mark.parametrize("spells,spell_cast_order,turns,expected_stats", spells_cases)
+def test_cast_spell(spells, spell_cast_order, turns, expected_stats):
     # Arrange
-    spells = [Spell(name="Magic Misile", mana_cost=53, effect=Effect(damage=4))]
-    player = Wizard(spells=spells)
+    player = Wizard(spells=spells, spell_cast_order=spell_cast_order)
     boss = Player()
     arena = MatchV2(player, boss)
     # Act
-    arena.run_turn()
+    [arena.run_turn() for _ in range(turns)]
     # Assert
-    assert player.mana == 447
-    assert boss.hp == 96
+    actual_stats = {"Boss": asdict(boss), "Player": asdict(player)}
+    for player, stats in expected_stats.items():
+        for stat in stats:
+            assert expected_stats[player][stat] == actual_stats[player][stat]
 
 
-def test_cast_healing_spell():
-    pass
+def test_sum_effects():
+    # Arrange
+    effect_a = Effect(damage=4)
+    effect_b = Effect(damage=2, heals=2)
+    effect_c = Effect(armor=2)
+    effect_d = Effect()
+    effect_e = Effect(mana=50)
+    # Act and Assert
+    assert effect_a + effect_b == Effect(damage=6, heals=2)
+    assert effect_b + effect_c == Effect(damage=2, heals=2, armor=2)
+    assert effect_a + effect_b + effect_c == Effect(damage=6, heals=2, armor=2)
+    effect_d += effect_b
+    assert effect_d == effect_d
+    assert effect_d + effect_e == Effect(damage=2, heals=2, mana=50)
